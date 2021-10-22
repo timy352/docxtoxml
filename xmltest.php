a05fd52263b75fada29233a799503d6e3232cddf
Timothy Edwards <91966621+timy352@users.noreply.github.com>
2021-10-22 20:49:17 +0100

    Add files via upload
    A file to test the php class

---
+++ xmltest.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+</head>
+
+<body>
+<?php
+require_once('wordxml.php');
+$rt = new WordPHP(false);
+$rt->readDocument('sample.docx');
+?>
+</body>
